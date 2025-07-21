f36d62069a863dd128773901a2bb53df7b691c74
Manshikatiyar <nishukatiyar97@gmail.com>
2025-07-21 12:25:12 +0530

    adding dev project data

---
+++ portfolio/src/Shared/Data.js
@@ -0,0 +1,42 @@
+
+
+export const DevelopmentProjects = [
+  // {
+  //   id: 1,
+  //   title: "Beat Policing Diary ",
+  //   desc: "A Web Application for the Admin side for Tripura Police hackathon. It is integrated with an Android App.",
+  //   stack: "React Js, Firebase, Node Js, Material UI",
+  //   link: "https://github.com/2058govindsingh/BPD-App/tree/master/BPD-App-bpdweb",
+  //   image: require("../assets/developmentProjects/bpd.png"),
+  //   color: "#CDF0EA",
+  // },
+  {
+    id: 2,
+    title: "Expense Tracker",
+    desc: " Resolved the process of expense tracking for the user",
+    stack: " ReactJs, NodeJS JavaScript, HTML, CSS and Material UI.",
+    link: "https://github.com/yashpal-97/Daily-Expense-Tracker",
+    image: require("../assets/developmentProjects/expense.png"),
+    color: "#FAF4B7",
+  },
+  {
+    id: 3,
+    title: "Job_Portal",
+    desc: "A job portal website connects job seekers with employers by listing job openings and allowing applications online.",
+    stack: " JavaScript, HTML, CSS and react.js.",
+    link: "https://github.com/Manshikatiyar/job-portal",
+    image: require("../assets/developmentProjects/password.png"),
+    color: "#FAF4B7",
+  },
+  {
+    id: 4,
+    title: "Coffe Cafee",
+    desc: "This website brings the aroma of Leone G Coffee Café to your screen — explore our brews, menu, and more.",
+    stack: " HTML, CSS and Tailwind",
+    link: "https://github.com/Manshikatiyar/Coffeecafe",
+    image: require("../assets/developmentProjects/webdesign.png"),
+    color: "#FAF4B7",
+  }
+ 
+];
+
